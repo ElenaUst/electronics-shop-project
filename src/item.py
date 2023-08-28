0961ce6e458a2b0a2941d938b73392c1da68ab1f
ElenaUst <elka_us@mail.ru>
2023-08-28 23:29:45 +0300

    add name getter and setter, instantiate_from_csv, string_to_number and tests for it

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import math
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +17,46 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all = self.all.append(self)
+        Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        """
+        Проверяет, что длина наименования товара не больше 10 симвовов. В противном случае, обрезает строку (оставляет первые 10 символов)
+        """
+        if len(value) > 10:
+            value = value[:10]
+        self.__name = value
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_
+        """
+        cls.all = []
+        with open(r"C:\Users\Lena\PycharmProjects\electronics-shop-project\src\items.csv", newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                read_name = row['name']
+                read_price = row['price']
+                read_quantity = row['quantity']
+                cls(read_name, read_price, read_quantity)
+
+
+    @staticmethod
+    def string_to_number(value):
+        """
+        Статический метод, возвращающий число из числа-строки
+        """
+        float_value = float(value)
+        return math.trunc(float_value)
 
 
     def calculate_total_price(self) -> float:
@@ -27,14 +67,8 @@ class Item:
         """
         return self.price * self.quantity
 
-
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= Item.pay_rate
-
-
-
-
-
--- tests/test_item.py
+++ tests/test_item.py
@@ -31,3 +31,26 @@ def test_item_initialized(item):
     assert item.name == "apple"
     assert item.price == 100
     assert item.quantity == 1000
+
+
+def test_name_setter(item):
+    """Тестируем name.setter"""
+    item.name = 'Холодильник'
+    assert item.name == 'Холодильни'
+
+
+def test_string_to_number():
+    """
+    Тестирует статический метод, возвращающий число из числа-строки
+    """
+    assert Item.string_to_number('11') == 11
+    assert Item.string_to_number('11.0') == 11
+    assert Item.string_to_number('11.5') == 11
+
+
+def test_instantiate_from_csv():
+    """
+    Тестирует Класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_
+    """
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
